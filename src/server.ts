ee11099ea0130e47de0ea7b8d2c8b14e786c75db
David González <angelglez961@gmail.com>
2024-10-02 21:20:36 -0600

    cors

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,15 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
+import { corsConfig } from './config/cors';
 import { connectDB } from './config/db';
 import tournamentRoutes from './routes/tournamentRoutes';
 
 dotenv.config();
-
 connectDB();
+
 const app = express();
+app.use(cors(corsConfig));
 
 app.use(express.json());
 
