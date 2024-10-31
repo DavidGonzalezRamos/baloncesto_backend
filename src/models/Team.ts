2e5deb30d6265750bf81368b5804fa716a8e1d9c
David González <angelglez961@gmail.com>
2024-10-31 13:53:50 -0600

    Poder agregar mismos equipos en diferentes torneos

--- src/models/Team.ts
+++ src/models/Team.ts
@@ -10,15 +10,19 @@ export interface ITeam extends Document  {
 }
 
 export const TeamSchema: Schema = new Schema({
-  nameTeam: { type: String, required: true, trim: true, unique: true },
+  nameTeam: { type: String, required: true, trim: true },
   branchTeam: { type: String, required: true, trim: true },
   nameCoach: { type: String, required: true, trim: true },
   tournament: {
     type: Types.ObjectId,
     ref: 'Tournament',
+    required: true
   },
   players: [{ type: Types.ObjectId, ref: 'Player' }]
 }, {timestamps: true}) 
+
+// Crear un índice compuesto para que 'nameTeam' sea único solo dentro del mismo 'tournament'
+TeamSchema.index({ nameTeam: 1, tournament: 1 }, { unique: true });
 
 const Team = mongoose.model<ITeam>('Team', TeamSchema)
 export default Team
